1bd1c6adfd8458b901b84045c1e733fa3cd2ca05
Coder <fundayelmo@gmail.com>
2026-03-13 15:17:24 +0100

    initial commit

---
+++ MovieApp-API.js
@@ -0,0 +1,25 @@
+// index.js
+const express = require('express');
+const app = express();
+
+// Demo movie data
+const movies = [
+  { id: 1, title: 'Inception' },
+
+];
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is working!');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(movies);
+});
+
+// Dynamic port for deployment
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at port ${PORT}`);
+});
